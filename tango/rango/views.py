5536bf2cfb47b319cb0cdeab4a751e96b614fee3
Prateek Narang <prateekrishu@yahoo.com>
2015-03-23 12:01:12 +0530

    added templates

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rango.models import Category
 
 # Create your views here.
 def index(request):
 	#response = " Rango says: Hello world! <br/> <a href='/rango/about'>About</a>"
-	context_dic ={'boldmessage':"This is bold text"}
-	return render(request,'rango/index.html',context_dic)
+	#context_dic ={'boldmessage':"This is bold text"}
+	category_list = Category.objects.all()
+	context_dict ={'categories':category_list}
+	return render(request,'rango/index.html',context_dict)
 	#return HttpResponse(response)
 
 def about(request):
