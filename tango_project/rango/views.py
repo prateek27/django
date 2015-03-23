013499cf085084d127b94c4108bea86f94df6830
Prateek Narang <prateekrishu@yahoo.com>
2015-03-24 01:38:13 +0530

    updates forms and views

--- tango_project/rango/views.py
+++ tango_project/rango/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from rango.models import Category,Page
 from rango.forms import CategoryForm,PageForm
@@ -15,7 +15,7 @@ def category(request,category_name):
 	#required_pages = Page.objects.filter(id=1)
 	c = Category.objects.get(name=category_name)
 	required_pages = c.page_set.all #c.Page.set_all()
-	context_dict = {"page_objects":required_pages}
+	context_dict = {"page_objects":required_pages,"category_name":category_name}
 	return render(request,'rango/category.html',context_dict)
 
 def about(request):
@@ -33,3 +33,28 @@ def add_category(request):
 	else:
 		form = CategoryForm()
 	return render(request,'rango/add_category.html',{'form':form})
+
+def add_page(request,category_name):
+	cat = Category.objects.get(name=category_name)
+	"""try:
+		cat = Category.objects.get(name=category_name)
+	except Category.DoesNotExist:
+		cat = None"""
+
+	if request.method == 'POST':
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request,category_name)
+
+		else:
+			print form.errors
+
+	else:
+		form = PageForm()
+		context_dict = {'form':form,'category':cat}
+		return render(request,'rango/add_page.html',context_dict)
+
