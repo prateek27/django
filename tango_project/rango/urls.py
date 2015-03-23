3e72fcb75b762c2d09be04438c19dfa7b89b0095
Prateek Narang <prateekrishu@yahoo.com>
2015-03-23 20:26:40 +0530

    categories and pages with argument passing

--- tango_project/rango/urls.py
+++ tango_project/rango/urls.py
@@ -4,4 +4,5 @@ from rango import views
 urlpatterns = patterns('',
 	url(r'^$',views.index,name='index'),
 	url(r'^about/',views.about,name='about'),
+	url(r'^category/(?P<category_name>[\w\-]+)/$', views.category, name='category')
 	)
--- tango_project/rango/views.py
+++ tango_project/rango/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category,Page
 
 # Create your views here.
 def index(request):
-	mykeys = {'myname':'Prateek'}
-	return render(request,'rango/index.html',mykeys)
+	#mykeys = {'myname':'Prateek'}
+	my_categories = Category.objects.all()
+	context_dict = {"category_list":my_categories}
+	return render(request,'rango/index.html',context_dict)
 	#return HttpResponse("Hello World !") 
 
+def category(request,category_name):
+	
+	#required_pages = Page.objects.filter(id=1)
+	c = Category.objects.get(name=category_name)
+	required_pages = c.page_set.all #c.Page.set_all()
+	context_dict = {"page_objects":required_pages}
+	return render(request,'rango/category.html',context_dict)
 
 def about(request):
 	return render(request,'rango/about.html',{})
